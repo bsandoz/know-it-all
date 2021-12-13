59a18a0cc03450ba2b0a891653795a83b738f04e
bsandoz <78384253+bsandoz@users.noreply.github.com>
2021-12-13 11:40:32 +0100

    Seitenstruktur angepasst, erster Schritt Kategorieübersicht

--- js/main.js
+++ js/main.js
@@ -1,71 +1,25 @@
-var kategorieArray = [];
-// var postNumber;
-
-fetch("https://174836-19.web1.fh-htwchur.ch/wp-json/wp/v2/posts")
+fetch("https://174836-19.web1.fh-htwchur.ch/wp-json/wp/v2/categories")
 //https://174836_19.web1.fh-htwchur.ch/wp-json/acf/v3/posts/
 	.then((response) => response.json())
 	//.then(data => console.log(data))
-	.then((data) => writePosts(data));
+	.then((data) => writeCategories(data));
 
-//geht durch das Array von fetch und ruft die Funktionen PostsAnzeigen und customField für jeden Post auf.
-function writePosts(arrayMitAllenPosts) {
-	//console.log(arrayMitAllenPosts);
-	//Woher kommt onePosts?
-	arrayMitAllenPosts.forEach((onePosts, i) => {
-		customField(onePosts.id);
-		//customFieldsAnzeigen(kategorieData);
-		setTimeout(() => { PostsAnzeigen(onePosts.title.rendered, onePosts.content.rendered, kategorieArray[i], onePosts.id); }, 2000);
+function writeCategories (arrayMitAllenCategories) {
+  arrayMitAllenCategories.forEach((oneCategories, i) => {
+    console.log(oneCategories.id);
+    categoriesAnzeigen(oneCategories.name, oneCategories.id);
 	});
+}
 
-	}
 
-//erstellt ein card-Div mit Titel und Content und hängt die Elemente an den container an.
-function PostsAnzeigen(title, content, category, id) {
-	const card = document.createElement("div");
+function categoriesAnzeigen(title, id) {
+  const card = document.createElement("div");
 	card.setAttribute("class", "card");
 	const h1 = document.createElement("h1");
 	h1.textContent = title;
-	const p = document.createElement("p");
-	p.innerHTML = content;
-	const kategorie = document.createElement("p");
-	kategorie.innerHTML = category;
-	container.appendChild(card);
+  container.appendChild(card);
 	card.appendChild(h1);
-	card.appendChild(kategorie);
-	card.appendChild(p);
-	card.addEventListener("click", function() {
-		location.href='https://174836-21.web1.fh-htwchur.ch/php/model.html?postid=' + id;
+  card.addEventListener("click", function() {
+		location.href='https://174836-21.web1.fh-htwchur.ch/subsites/category.html?categoryid=' + id;
 	})
-	console.log(kategorieArray);
 }
-
-//holt die custom fields jedes Post anhand der id und ruft die Funktion writeCustomField auf.
-function customField(id){
-	console.log("https://174836-19.web1.fh-htwchur.ch/wp-json/acf/v3/posts/"+id);
-	var url = "https://174836-19.web1.fh-htwchur.ch/wp-json/acf/v3/posts/"+id;
-	fetch(url)
-	//https://629120-4.web1.fh-htwchur.ch/wp-json/acf/v3/posts/
-		.then((response) => response.json())
-		//.then(data => console.log(data))
-		.then((data) => writeCustomField(data));
-
-}
-
-//Gibt das custom field in der Konsole aus
-function writeCustomField(data){
-	//console.log(data.acf.kategorie);
-	kategorieArray.push(data.acf.kategorie);
-	console.log(kategorieArray);
-	const kategorie = document.createElement("p");
-	kategorie.innerHTML = data.acf.kategorie;
-	//container.appendChild(kategorie);
-	//console.log(data.acf.kurzbeschreibung);
-}
-
-/*
-function customFieldsAnzeigen(input) {
-	console.log(input);
-	kategorieField = document.createElement("p");
-	kategorieField.innerHTML = input;
-}
-*/
